50af47e8a02870926a58ec757a75f8e7fa873876
Brian Thomas <btdeveloper125@gmail.com>
2021-11-17 12:54:48 -0700

    refactor: Setup name state

--- componets/Start.js
+++ componets/Start.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 class Start extends Component {
   constructor(props){
     super(props)
     this.state = {
-      
+      name:''
     }
   }
   render() {
     return (
       <View style={styles.container}>
         <Text>hello</Text>
+        <TextInput 
+          onChangeText={(text) => this.setState({name: text})}
+        />
         <Button 
           title='Go to chat'
-          onPress={() => this.props.navigation.navigate('Chat')}
+          onPress={() => this.props.navigation.navigate('Chat', {name: this.state.name})}
         />
       </View>
     );
