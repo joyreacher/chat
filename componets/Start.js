ed8e073d50bfc8e5e0a61793726d25954181c34c
Brian Thomas <btdeveloper125@gmail.com>
2021-11-17 14:05:50 -0700

    refactor: Setup base layout

--- componets/Start.js
+++ componets/Start.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ImageBackground, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TextInput, Button, Pressable } from 'react-native';
 // background image
 const image = require('../assets/project_assets/bg.png')
 class Start extends Component {
@@ -17,15 +17,26 @@ class Start extends Component {
         resizeMode='cover'
         style={styles.image}
       >
-        <Text>hello</Text>
+      
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Chat</Text>
+        </View>
+          
+        <View style={styles.main}>
           <TextInput 
-            style={styles.input}
-            onChangeText={(text) => this.setState({name: text})}
-          />
-          <Button 
-            title='Go to chat'
-            onPress={() => this.props.navigation.navigate('Chat', {name: this.state.name})}
-          />
+                style={styles.input}
+                onChangeText={(text) => this.setState({name: text})}
+              />
+        </View>
+        <View style={styles.cta}>
+          <Pressable style={styles.button} onPress={() => this.props.navigation.navigate('Chat', {name: this.state.name})}>
+                <Text
+                  style={styles.text}
+                >Go to Chat</Text>
+              </Pressable>
+        </View>
+      
+      
       </ImageBackground>
       </View>
     );
@@ -37,15 +48,50 @@ export default Start;
 const styles = StyleSheet.create({
   container: {
     flex:1,
+    justifyContent:'space-around',
+    flexDirection:'column',
+  },
+  
+  header:{
+    flex:1,
+    position:'relative',
+    alignSelf:'stretch',
+    top:0,
+    maxHeight:'10%',
+    width:'auto',
+    paddingTop:15,
+    paddingLeft:20
+  },
+  headerText:{
+    color:'white',
+    height:100,
+    fontSize: 25
+  },
+  main:{
+    height:100
+  },
+  cta:{
+    marginBottom:150
+  },
+  text:{
+    color:'white',
+    fontSize:35
   },
   input:{
-    backgroundColor:'red',
+    borderRadius:5,
+    backgroundColor:'white',
     width:300,
     height:50
   },
   image: {
     flex:1,
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems:'center',
+    
+  },
+  button:{
+    backgroundColor:'green',
+    padding:10,
+    borderRadius:10
   }
 });
