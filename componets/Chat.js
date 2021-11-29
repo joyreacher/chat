56b9885b8d63e498187b7042aa2778faf06b878c
Brian Thomas <btdeveloper125@gmail.com>
2021-11-29 11:05:12 -0700

    refactor: Add base styles to chat screen

--- componets/Chat.js
+++ componets/Chat.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 // react native specific components
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 class Chat extends Component {
   constructor(props){
     super()
@@ -12,25 +12,26 @@ class Chat extends Component {
     // Add the name to top of screen
     this.props.navigation.setOptions({ title: name})
     return (
-      <View style={{
-        backgroundColor: color,
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-        <Text>CHAT</Text>
+      <View style={[{backgroundColor: color}, styles.container]}>
+        <Text
+          style={styles.text}
+        >CHAT</Text>
         {/* using the 'name' container */}
-        <Text>
-          {name}
-          {color.color}
+        <Text
+          style={styles.text}
+        >
+          Hello {name},
+          You Chose {color} as your background.
         </Text>
-        <Button
+        <Pressable
+          style={styles.button}
           title='Go back'
           // calls on the navigation's navigate prop.
           // Navigates you to 'Start'
           onPress={() => this.props.navigation.navigate('Start')}
         >
-        </Button>
+        <Text style={styles.buttonText}>Go Back</Text>
+        </Pressable>
       </View>
     );
   }
@@ -41,8 +42,20 @@ export default Chat;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'rgb(255, 255, 255)'
+  },
+  button:{
+    width: 100,
+    height: 50,
+    backgroundColor:'#757082'
+  },
+  buttonText:{
+    paddingTop:15,
+    textAlign:'center',
+    color: 'rgb(255, 255, 255)'
+  }
 });
