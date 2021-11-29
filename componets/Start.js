343addc62d02f18a2486e0f9d182a28e70ca8cbf
Brian Thomas <btdeveloper125@gmail.com>
2021-11-29 09:44:01 -0700

    refactor: Update app's UI

--- componets/Start.js
+++ componets/Start.js
@@ -22,62 +22,23 @@ class Start extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Modal
-          animationType="fade"
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {
-            Alert.alert("Modal has been closed.");
-            this.setState(!this.state.modalVisible);
-          }}
-        >
-        {/* main modal section */}
-        <View style={styles.innerModal}>
-        
-          {/* close button */}
-          <View style={[styles.selectionContainer, styles.header]}>
-            <Text>Set UI Background</Text>
-            <Button
-              title='Close'
-              onPress={() => this.handleModal()}
-            />
-          </View>
-
-          {/* color selction */}
-          <View style={styles.colorSelectionContainer}>
-            <View 
-              style={styles.purple}
-            />
-            <View
-              style={styles.orange}
-            />
-          </View>
-          
-        </View>
-        </Modal>
       <ImageBackground
         source={image}
         resizeMode='cover'
         style={styles.image}
       >
-        {/* navbar */}
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Chat</Text>
-          <Pressable onPress={() => this.handleModal()}>
-            <Text style={styles.headerText}>Colors</Text>
-          </Pressable>
-          
-        </View>
-
-          {/* input */}
+        {/* input */}
         <View style={styles.main}>
-          <TextInput 
-                style={styles.input}
-                onChangeText={(text) => this.setState({name: text})}
-              />
+          <Text style={styles.title}>
+            Chat
+          </Text>
         </View>
         {/* call to action */}
         <View style={styles.containerCta}>
+        <TextInput 
+                style={styles.input}
+                onChangeText={(text) => this.setState({name: text})}
+              />
           <View style={styles.cta}>
             <Pressable style={styles.button} onPress={() => this.props.navigation.navigate('Chat', {name: this.state.name})}>
                 <Text
@@ -104,7 +65,7 @@ const styles = StyleSheet.create({
     width:300,
     alignSelf:'center',
     padding:23,
-    backgroundColor:'black',
+    backgroundColor:'white',
     shadowOpacity:.5,
     shadowRadius:7,
     shadowColor:'black',
@@ -114,42 +75,9 @@ const styles = StyleSheet.create({
     },
     borderRadius:22
   },
-  colorSelectionContainer:{
-    marginTop:100,
-    flex:1,
-    flexDirection:'row',
-    justifyContent:'space-evenly',
-    flexWrap:'wrap',
-    width:'100vw',
-    width:300,
-    maxWidth:'100%'
-  },
-  selectionContainer:{
-    flex:1,
-    alignItems: "center",
-    flexDirection:'row',
-    justifyContent: 'space-between',
-    marginBottom:100
-  },
   container: {
     flex:1,
     justifyContent:'space-around',
-  },
-  innerModal:{
-    flex:1,
-    // flexDirection:'row',
-    margin: 20,
-    borderRadius: 20,
-    padding:25,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
   },
   centeredView: {
     flex: 1,
@@ -158,26 +86,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: 22
   },
-  header:{
-    flex:1,
-    flexDirection:'row',
-    alignItems:'center',
-    position:'relative',
-    alignSelf:'stretch',
-    justifyContent:'space-between',
-    top:0,
-    maxHeight:'10%',
-    width:'100%',
-    paddingTop:15,
-    paddingLeft:20,
-    paddingRight:20
-  },
-  headerText:{
-    color:'white',
-    fontSize: 25
-  },
   main:{
-    height:100
+    height:100,
+    flex:1,
+    justifyContent:'center'
   },
   cta:{
     marginBottom:150
@@ -187,10 +99,13 @@ const styles = StyleSheet.create({
     color:'black',
     fontSize:35
   },
+  title:{
+    color:'white',
+    fontSize: 56
+  },
   input:{
     borderRadius:5,
     backgroundColor:'white',
-    width:300,
     height:50
   },
   image: {
