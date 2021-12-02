5d10813907b968a823282ce2fda320ebe0400f44
Brian Thomas <btdeveloper125@gmail.com>
2021-12-02 09:39:10 -0700

    refactor: Setup Chat screen for Gifted chat UI

--- componets/Chat.js
+++ componets/Chat.js
@@ -13,6 +13,9 @@ import { GiftedChat } from 'react-native-gifted-chat'
 class Chat extends Component {
   constructor(props){
     super()
+    /**
+     * Messages state initialized with empty array
+     */
     this.state = {
       messages: []
     }
@@ -24,6 +27,23 @@ class Chat extends Component {
       !IMPORTANT setOptions needs to be in component did mount to avoid console errors
   */
   componentDidMount() {
+    /**
+      When using Gifted Chat, each message needs to have an ID, creation date, and user object
+      The user object requires user ID, name, and avatar.
+      More https://github.com/FaridSafi/react-native-gifted-chat
+     */
+    this.setState({
+      messages:[{
+        _id:1,
+        text: 'Hello Developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any'
+        }
+      }]
+    })
     const { name } = this.props.route.params
     this.props.navigation.setOptions({ title: name })
   }
@@ -31,75 +51,16 @@ class Chat extends Component {
     // store the prop values that are passed
     let { name, color } = this.props.route.params
     return (
-      <View style={[{backgroundColor: color}, styles.container]}>
-        {/* App Title */}
-        <View style={styles.main}>
-          <Text style={styles.title}>CHAT</Text>
-        </View>
-        <View style={styles.main}>
-          <View style={styles.responseContainer}>
-            {/* Main Text */}
-            <Text style={styles.text}>
-              Hello <Text style={styles.data}>{name}</Text>,
-              You Chose <Text style={styles.data}>{color}</Text> as your background.
-            </Text>
-          </View>
-          {/* Go Back Button */}
-          <View style={styles.main}>
-            <Pressable
-              style={styles.button}
-              title='Go back'
-              /**
-                Calls on the navigation's navigate prop to navigates you back to 'Start'
-               */
-              onPress={() => this.props.navigation.navigate('Start')}
-            >
-            <Text style={styles.buttonText}>Go Back</Text>
-            </Pressable>
-          </View>
-        </View>
-      </View>
-    );
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id:1
+        }} />
+    )
   }
 }
 
 export default Chat;
 
-const styles = StyleSheet.create({
-  responseContainer:{
-    padding:5
-  },
-  main:{
-    height: 100,
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
-  },
-  data:{
-    color: '#FFFFFF'
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#757083',
-    fontSize: 16
-  },
-  title: {
-    color:'#FFFFFF',
-    fontSize: 45,
-    fontWeight: '600'
-  },
-  button:{
-    width: 100,
-    height: 50,
-    backgroundColor:'#757082'
-  },
-  buttonText:{
-    paddingTop:15,
-    textAlign:'center',
-    color: 'rgb(255, 255, 255)'
-  }
-});
+const styles = StyleSheet.create({});
